dff56569021caf4cbe16a100be1819969dea2c4b
jleviharris <96702475+jleviharris@users.noreply.github.com>
2022-09-19 15:58:27 -0400

    Day 3 of 100 day of javascript complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [hexNumber, setHexNumber] = useState(null);
+
+  const generateNumber = () => {
+    setHexNumber(randm);
+  };
+  const randm = Math.random().toString(16).substring(2, 8);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Hex Color Generator</h1>
+      <div className="colors">
+        {" "}
+        <div className="counter">#{hexNumber}</div>
+        <div className="colorBox" style={{ background: `#${hexNumber}` }}></div>
+      </div>
+
+      <br />
+
+      <button className="Bttn" onClick={generateNumber}>
+        Generate Color
+      </button>
     </div>
   );
 }
